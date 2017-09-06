d107b828e761d6969e4d210238118f3a59e5b560
tobia <rafaelmontas96@gmail.com>
2017-09-06 03:23:23 -0400

    Pages colors, Pages Added and menu already implemented

---
+++ ECommerce/ECommerce/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infraestructure
+{
+  public  class InstanceLocator
+    {
+
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ ECommerce/ECommerce/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.ViewModels
+{
+   public class MainViewModel
+    {
+        #region Properties
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+        #endregion
+
+
+        #region Constructors
+        public MainViewModel()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+            LoadMenu();
+        }
+
+        #endregion
+
+        #region Methods
+        private void LoadMenu()
+        {
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "product.png",
+                PageName = "ProductsPage",
+                Title = "Products",
+            });
+
+            Menu.Add(new MenuItemViewModel
+                {
+                Icon = "user.png",
+                PageName = "CustomersPage",
+                Title = "Customers",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "order.png",
+                PageName = "OrdersPage",
+                Title = "Orders",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "delivery.png",
+                PageName = "DeliveriesPage",
+                Title = "Deliveries",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "sync.png",
+                PageName = "SyncPage",
+                Title = "Sync",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "setup.png",
+                PageName = "SetupPage",
+                Title = "Setup",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "logout.png",
+                PageName = "LogOut",
+                Title = "Logout",
+            });
+
+
+
+
+        }
+        #endregion
+
+    }
+}
